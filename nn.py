0519a429a588dd1a50f27dfa0306693ea3e79afe
Darren Kessner <dkessner@eitm.org>
2023-02-18 14:12:03 -0800

    nn: added class Network

--- nn.py
+++ nn.py
@@ -27,25 +27,77 @@ class LinearTransformation(VectorFunction):
         return self.A @ x
 
 
+class Network(VectorFunction):
+    def __init__(self, T):
+        self.T = T # list of transformations (VectorFunction objects)
 
-def main():
+        # sanity check
+        for i, t in enumerate(T):
+            assert isinstance(t, VectorFunction)
+            if i>0:
+                assert T[i-1].len_out == t.len_in
 
+        super().__init__(T[0].len_in, T[-1].len_out)
+        
+    def __call__(self, x):
+        # apply each transformation in the list
+        result = x
+        for t in self.T:
+            result = t(result)
+        return result
+
+
+def test_vector_function():
+    print("test_vector_function()")
     f = VectorFunction(3, 4)
     x = np.array([1, 3, 5])
-    print("x:", x)
-    print("f(x):", f(x))
+    assert (f(x) == np.zeros(4)).all()
 
+
+def test_linear_transformation():
+    print("test_linear_transformation()")
     A = np.array([[1, 3, 5], [2, 4, 6]])
-    print("A:", A)
+    print("A:\n", A)
     T = LinearTransformation(A)
-    e1 = np.array([1, 0, 0])
-    e2 = np.array([0, 1, 0])
-    e3 = np.array([0, 0, 1])
-    print("T(e1):", T(e1))
-    print("T(e2):", T(e2))
-    print("T(e3):", T(e3))
+    e = np.eye(3)
+    print("T(e[0]):", T(e[0]))
+    print("T(e[1]):", T(e[1]))
+    print("T(e[2]):", T(e[2]))
+    assert (T(e[0]) == [1,2]).all()
+    assert (T(e[1]) == [3,4]).all()
+    assert (T(e[2]) == [5,6]).all()
 
 
+def test_network():
+    print("test_network()")
+
+    A = np.array([[1,2],[3,4],[5,6]])
+    B = np.eye(3) * 2
+
+    n = Network([LinearTransformation(A), 
+                 LinearTransformation(B)])
+
+    assert n.len_in == 2
+    assert n.len_out == 3
+
+    e = np.eye(2)
+    print(n(e[0]))
+    print(n(e[1]))
+    assert (n(e[0]) == [2,6,10]).all()
+    assert (n(e[1]) == [4,8,12]).all()
+    print(n(e))
+    assert (n(e) == [[2,4], [6,8], [10,12]]).all()
+
+
+
+def run_tests():
+    test_vector_function()
+    test_linear_transformation()
+    test_network()
+
+
+def main():
+    run_tests()
 
 
 if __name__ == '__main__':
