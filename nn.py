2ef52a9707e6ecf2e4d8369c5f367a8dee616ecf
drkessner hp <Darren.Kessner@marlborough.org>
2023-02-21 08:13:10 -0800

    added SimpleLinearNetwork

--- nn.py
+++ nn.py
@@ -18,13 +18,35 @@ class VectorFunction:
         return np.zeros(self.len_out)
 
 
+def test_vector_function():
+    print("test_vector_function()")
+    f = VectorFunction(3, 4)
+    x = np.array([1, 3, 5])
+    assert (f(x) == np.zeros(4)).all()
+
+
 class LinearTransformation(VectorFunction):
     def __init__(self, A):
         self.A = A
+        print("A.shape:", A.shape)
         len_out, len_in = A.shape
         super().__init__(len_in, len_out)
     def __call__(self, x):
         return self.A @ x
+
+
+def test_linear_transformation():
+    print("test_linear_transformation()")
+    A = np.array([[1, 3, 5], [2, 4, 6]])
+    print("A:\n", A)
+    T = LinearTransformation(A)
+    e = np.eye(3)
+    print("T(e[0]):", T(e[0]))
+    print("T(e[1]):", T(e[1]))
+    print("T(e[2]):", T(e[2]))
+    assert (T(e[0]) == [1,2]).all()
+    assert (T(e[1]) == [3,4]).all()
+    assert (T(e[2]) == [5,6]).all()
 
 
 class Network(VectorFunction):
@@ -45,28 +67,6 @@ class Network(VectorFunction):
         for t in self.T:
             result = t(result)
         return result
-
-
-def test_vector_function():
-    print("test_vector_function()")
-    f = VectorFunction(3, 4)
-    x = np.array([1, 3, 5])
-    assert (f(x) == np.zeros(4)).all()
-
-
-def test_linear_transformation():
-    print("test_linear_transformation()")
-    A = np.array([[1, 3, 5], [2, 4, 6]])
-    print("A:\n", A)
-    T = LinearTransformation(A)
-    e = np.eye(3)
-    print("T(e[0]):", T(e[0]))
-    print("T(e[1]):", T(e[1]))
-    print("T(e[2]):", T(e[2]))
-    assert (T(e[0]) == [1,2]).all()
-    assert (T(e[1]) == [3,4]).all()
-    assert (T(e[2]) == [5,6]).all()
-
 
 def test_network():
     print("test_network()")
@@ -89,6 +89,20 @@ def test_network():
     assert (n(e) == [[2,4], [6,8], [10,12]]).all()
 
 
+class SimpleLinearNetwork(Network):
+    def __init__(self, m=0, b=0):
+        T = LinearTransformation(np.array([[b, m]]))
+        super().__init__([T])
+
+
+def test_simple_linear_network():
+    l = SimpleLinearNetwork(2, 3)
+    x = np.array(range(11))
+    xt = np.array([np.ones_like(x), x]) # xt = (x)' (2 row vectors)
+    y = l(xt)
+    print("xt:", xt)
+    print("y:", y)
+
 
 def run_tests():
     test_vector_function()
@@ -97,7 +111,7 @@ def run_tests():
 
 
 def main():
-    run_tests()
+    test_simple_linear_network()
 
 
 if __name__ == '__main__':
