d8e4825db5108125a1def3b267bd82209d943a6b
Darren Kessner <dkessner@eitm.org>
2023-03-05 11:07:33 -0800

    logistic_regression += randomly generated data

--- logistic_regression.py
+++ logistic_regression.py
@@ -15,14 +15,26 @@ from numpy import exp, log
 #   https://en.wikipedia.org/wiki/Logistic_regression
 #
 
+def wikipedia_example():
+    x = np.array([0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 1.75, 2.00, 2.25, 2.50, 2.75, 3.00, 3.25, 3.50, 4.00, 4.25, 4.50, 4.75, 5.00, 5.50])
+    y = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
+    return x, y
 
-x = np.array([0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 1.75, 2.00, 2.25, 2.50, 2.75, 3.00, 3.25, 3.50, 4.00, 4.25, 4.50, 4.75, 5.00, 5.50])
-y = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
 
 # general logistic function
 
 def sigma(t, b0, b1):
     return 1/(1 + exp(-b0+(-b1)*t))
+
+def bernoulli(p):
+    return np.random.binomial(1, p) 
+
+def generate_data(b0, b1):
+    x = np.linspace(0, 10, 101)
+    sx = sigma(x, b0, b1)
+    y = np.array([bernoulli(p) for p in sx])
+    return x, y
+
 
 # log-likelihood and gradient
 
@@ -39,18 +51,15 @@ def dLdb1(x, y, b0, b1):
     return (x*(y-s)).sum()
 
 
+def gradient_descent(x, y):
 
-def main():
-
-    print(x, y)
-
-    b0 = 0
-    b1 = 1
-    rate = .05
+    b0, b1 = 0, 1
+    rate = .005
     epsilon = 1e-8
 
     for i in range(5000):
-        print(b0, b1, L(x,y,b0,b1))
+        if i%100 == 0:
+            print(b0, b1, L(x, y, b0, b1))
         db0 = dLdb0(x, y, b0, b1) * rate
         db1 = dLdb1(x, y, b0, b1) * rate
         b0 += db0
@@ -59,10 +68,24 @@ def main():
             print("iterations:", i+1)
             break
 
+    return b0, b1
+
+
+def main():
+
+    b0 = -5
+    b1 = 1
+
+    #x, y = wikipedia_example()
+    x, y = generate_data(b0, b1)
+    print(x, y)
+
+    b0, b1 = gradient_descent(x, y)
+    print("(b0,b1):", b0, b1)
+
     plt.plot(x, y, '.')
     plt.plot(x, sigma(x, b0, b1), '-')
     plt.show()
-
 
 
 
